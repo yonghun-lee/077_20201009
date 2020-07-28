af5538fd5a5c189ac52de3d8c4720a49786d938b
multikyd <55026351+multikyd@users.noreply.github.com>
2020-07-28 16:20:42 +0900

    Update tune_lqr.py

--- tune_lqr.py
+++ tune_lqr.py
@@ -65,7 +65,7 @@ while True:
   print (print_letters(kyd.conf[param[j]]))
   print ("")
   print ("q: +50   w: +1   1: +0.1   3: +0.05   5: +0.01   7: +0.001   r: +0.00001")
-  print ("a: -50   x: -1   a: -0.1   d: -0.05   g: -0.01   j: -0.001   v: -0.00001")
+  print ("c: -50   x: -1   a: -0.1   d: -0.05   g: -0.01   j: -0.001   v: -0.00001")
 #  print ("w,1,3,5,7,r to incr 1,0.1,0.05,0.01,0.001,0.00001")
 #  print ("x,a,d,g,j,v to decr 1,0.1,0.05,0.01,0.001,0.00001")
   print ("0: 0   L: 1")
@@ -127,7 +127,7 @@ while True:
     kyd.conf[param[j]] = str(round((float(kyd.conf[param[j]]) - 1),5))
     write_json = True
 
-  elif (char == "a"):
+  elif (char == "c"):
     kyd.conf[param[j]] = str(round((float(kyd.conf[param[j]]) - 50),5))
     write_json = True
 
