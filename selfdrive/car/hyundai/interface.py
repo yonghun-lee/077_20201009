4d8a1b580cf67262bd2507242d617b2cbe10ee98
multikyd <multikyd@gmail.com>
2020-07-28 14:43:18 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -413,7 +413,7 @@ class CarInterface(CarInterfaceBase):
     elif self.lateralControlMethod == 2:
       if candidate == CAR.SANTAFE:
         ret.lateralTuning.init('lqr')
-	      ret.lateralTuning.lqr.scale = 2000.0
+        ret.lateralTuning.lqr.scale = 2000.0
 	      ret.lateralTuning.lqr.ki = 0.01
 	      ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
 	      ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
