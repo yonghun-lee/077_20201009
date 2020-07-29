2b7ea31b32feb3fa93eaecf2cfd38ff916b04cf4
multikyd <multikyd@gmail.com>
2020-07-29 16:36:26 +0900

    Update hyundaican.py

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -73,7 +73,8 @@ def create_clu11(packer, frame, bus, clu11, button, speed = None):
   values = copy.deepcopy( clu11 )
   if speed != None:
     values["CF_Clu_Vanz"] = speed
-  values["CF_Clu_CruiseSwState"] = button
+
+  int(values["CF_Clu_CruiseSwState"]) = button
   values["CF_Clu_AliveCnt1"] = frame % 0x10
   return packer.make_can_msg("CLU11", bus, values)
 
