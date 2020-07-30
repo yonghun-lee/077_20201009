10e2516457ecec46d215a4bd859956d3d17583be
multikyd <multikyd@gmail.com>
2020-07-30 13:04:40 +0900

    update

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -781,8 +781,8 @@ class CarInterface(CarInterfaceBase):
     self.CS.out = ret.as_reader()
     return self.CS.out
 
-  def apply(self, c, sm, CP, VM ):
-    can_sends = self.CC.update(c, self.CS, self.frame, sm, CP, VM )
+  def apply(self, c, sm, CP ):
+    can_sends = self.CC.update(c, self.CS, self.frame, sm, CP )
 
     self.frame += 1
     return can_sends
