260c59775b45b8a13485b4b016390b378e5c8d61
multikyd <multikyd@gmail.com>
2020-08-04 14:42:08 +0900

    Update pathplanner.py

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -164,7 +164,7 @@ class PathPlanner():
       else:
         self.steerRatio = self.sR[0]
       
-      #print("steerRatio =", self.steerRatio)
+      print("steerRatio =", self.steerRatio)
 
     self.LP.parse_model(sm['model'])
 
