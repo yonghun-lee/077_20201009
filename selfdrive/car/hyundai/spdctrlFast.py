75e7a2e1c323343ea3130802933a7fe1f70a4dc9
multikyd <multikyd@gmail.com>
2020-08-06 15:18:01 +0900

    Update spdctrlFast.py

--- selfdrive/car/hyundai/spdctrlFast.py
+++ selfdrive/car/hyundai/spdctrlFast.py
@@ -132,8 +132,9 @@ class SpdctrlFast(SpdController):
                     lead_wait_cmd, lead_set_speed = self.get_tm_speed( CS, 20, 9)
 
             if dRel > (CS.clu_Vanz + lead_objspd) * self.cv_Raio :   # 선행차 속도를 감안한(가감속) "내차 주행 속도" 수치의 비율(cv_Raio) 보다 선행차가 멀리 있다면 가속할 수 있도록 최대 설정 속도로 설정
-                self.seq_step_debug = "최대가속"
-                lead_set_speed = self.cruise_set_speed_kph
+                self.seq_step_debug = "가속(+20)"
+                #lead_set_speed = self.cruise_set_speed_kph
+                lead_wait_cmd, lead_set_speed = self.get_tm_speed( CS, 10, 20)
 
         return lead_wait_cmd, lead_set_speed
 
